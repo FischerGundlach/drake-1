44f0808dc92ffd199a3de05f71c592830550b629
fischergundlach <m.j.c.fischer-gundlach@student.tudelft.nl>
2017-08-08 16:13:19 -0400

    compiled and unit tested

--- drake/math/continuous_lyapunov_equation.cc
+++ drake/math/continuous_lyapunov_equation.cc
@@ -1,17 +1,18 @@
 #include "drake/math/continuous_lyapunov_equation.h"
 
+#include <complex>
+#include <iostream>
 #include <stdexcept>
 
 #include "drake/common/drake_assert.h"
-#include "drake/common/eigen_types.h"
 #include "drake/common/is_approx_equal_abstol.h"
 
 namespace drake {
 namespace math {
 
-using drake::Vector1d;
 using Eigen::Matrix2d;
 using Eigen::MatrixXd;
+using Eigen::VectorXcd;
 
 MatrixXd ContinuousLyapunovEquation(const Eigen::Ref<const MatrixXd>& A,
                                     const Eigen::Ref<const MatrixXd>& Q) {
@@ -19,20 +20,30 @@ MatrixXd ContinuousLyapunovEquation(const Eigen::Ref<const MatrixXd>& A,
     throw std::runtime_error("A and Q must be square and of equal size!");
   }
 
-  // TODO(FischerGundlach) Support non-symmetric case?
-  if (!(is_approx_equal_abstol(Q(1), Q(2), 1e-10))) {
+  if (!(is_approx_equal_abstol(Q, Q.transpose(), 0))) {
     throw std::runtime_error("Q must be symmetric!");
   }
 
-  if (static_cast<int>(A.size()) == 1) {
-    return interal::Solve1By1ContinuousLyapunovEquation(A, Q);
-  } else if (static_cast<int>(A.size()) == 4) {
-    return interal::Solve2By2ContinuousLyapunovEquation(A, Q);
+  VectorXcd eig_val{A.eigenvalues()};
+  for (int i = 0; i < eig_val.size(); ++i) {
+    for (int j = i; j < eig_val.size(); ++j) {
+      if (std::norm(eig_val(i) + std::conj(eig_val(j))) == 0 ||
+          std::norm(std::conj(eig_val(i)) + eig_val(j)) == 0) {
+        throw std::runtime_error("Solution is not unique!");
+      }
+    }
+  }
+
+  if (static_cast<int>(A.cols()) == 1) {
+    return internal::Solve1By1ContinuousLyapunovEquation(A, Q);
+  } else if (static_cast<int>(A.cols()) == 2) {
+    return internal::Solve2By2ContinuousLyapunovEquation(A, Q);
   } else {
     // Transform into S'*X_bar + X_bar*S = -Q_bar, where A = U*S*U'.
     Eigen::RealSchur<MatrixXd> schur(A);
     MatrixXd U{schur.matrixU()};
     MatrixXd S{schur.matrixT()};
+
     DRAKE_DEMAND(is_approx_equal_abstol(U * S * U.transpose(), A, 1e-10));
     MatrixXd Q_bar{U.transpose() * Q * U};
 
@@ -45,32 +56,41 @@ namespace internal {
 
 using Eigen::Matrix3d;
 using Eigen::Vector3d;
+using Eigen::VectorXd;
 
 Vector1d Solve1By1ContinuousLyapunovEquation(
     const Eigen::Ref<const Vector1d>& A, const Eigen::Ref<const Vector1d>& Q) {
-  return Vector1d(Q(0) / (2 * A(0)));
+  std::cout << "Matrix A: " << A << std::endl;
+  std::cout << "Matrix Q: " << Q << std::endl;
+  return Vector1d(-Q(0) / (2 * A(0)));
 }
 
 Matrix2d Solve2By2ContinuousLyapunovEquation(
     const Eigen::Ref<const Matrix2d>& A, const Eigen::Ref<const Matrix2d>& Q) {
-  // TODO(FischerGundlach) Support non-symmetric case?
-  DRAKE_ASSERT(is_approx_equal_abstol(Q(1), Q(2), 1e-10));
-
-  // TODO(FischerGundlach) Use better a name for A_vec. It's not a vector!
-  // Rewrite A'X + XA + Q = 0 into linear set A_vec *vec(X) = -vec(Q) and
+  DRAKE_DEMAND(
+      is_approx_equal_abstol(Q.block(1, 0, 1, 1), Q.block(0, 1, 1, 1), 1e-10));
+  std::cout << "Matrix A: " << A << std::endl;
+  std::cout << "Matrix Q: " << Q << std::endl;
+  // Rewrite A'X + XA + Q = 0 (case where A,Q are 2-by-2) into linear set A_vec
+  // *vec(X) = -vec(Q) and
   // solve for X.
   Matrix3d A_vec;
-  A_vec << 2 * A(0), A(2), 0, 2 * A(1), A(0) + A(3), 2 * A(2), 0, A(1),
-      2 * A(3);
+  A_vec << 2 * A(0, 0), 2 * A(1, 0), 0, A(0, 1), A(0, 0) + A(1, 1), A(1, 0), 0,
+      2 * A(0, 1), 2 * A(1, 1);
   Vector3d Q_vec;
-  q << Q(0), Q(1), Q(2);
+  Q_vec << -Q(0, 0), -Q(0, 1), -Q(1, 1);
+
+  std::cout << "Vector A_vec: " << A_vec << std::endl;
+  std::cout << "Vector Q_vec: " << Q_vec << std::endl;
 
   // See https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html for
   // quick overview of possible solver algorithms.
   // ColPivHouseholderQR is accurate and fast for small systems.
-  Vector3d x{A_vec.colPivHouseHolderQr().solve(q)};
+  Vector3d x{A_vec.colPivHouseholderQr().solve(Q_vec)};
+  std::cout << "Vector x: " << x << std::endl;
   Matrix2d X;
   X << x(0), x(1), x(1), x(2);
+  std::cout << "Matrix X: " << X << std::endl;
 
   return X;
 }
@@ -78,39 +98,70 @@ Matrix2d Solve2By2ContinuousLyapunovEquation(
 MatrixXd SolveReducedContinuousLyapunovEquation(
     const Eigen::Ref<const MatrixXd>& S,
     const Eigen::Ref<const MatrixXd>& Q_bar) {
-  // catch the scalar case
-  if (static_cast<int>(S.size()) == 1) {
+  std::cout << "Matrix S: " << S << std::endl;
+  std::cout << "Matrix Q_bar: " << Q_bar << std::endl;
+  int m{static_cast<int>(S.rows())};
+  if (m == 1) {
     return Solve1By1ContinuousLyapunovEquation(S, Q_bar);
-  } else if (static_cast<int>(S.size()) == 2) {
+  } else if (m == 2) {
     return Solve2By2ContinuousLyapunovEquation(S, Q_bar);
   } else {
-    // Notation & partition adapted from SB03MD.
+    // Notation & partition adapted from SB03MD:
+    //        (s    s')      (q   q')      (x   x')
+    //        ( 11    )  _   ( 11   )  _   ( 11   )
+    //    S = (       ), Q = (      ), X = (      )
+    //        (       )      ( _    )      ( _    )
+    //        ( 0   S )      ( q  Q )      ( x  X )
+    //               1             1             1
+    // where s_11 is a 1-by-1 or 2-by-2 matrix.
+    //
+    //    s' x   + x  s   = -q                                       (3.1)
+    //     11 11    11 11     11
+    //
+    //      _   _            _    _
+    //    S'x + xs        = -q - sx                                  (3.2)
+    //     1      11              11
+    //
+    //    _    _
+    //    S'X + X S       = -Q - (sx' + xs')  = -Q                   (3.3)
+    //     1 1   1 1          1                   new
+
     int n{1};  // n equals the size of the n-by-n block in the left-upper
                // corner of S.
-    if (!(is_approx_equal_abstol(U.block(0, 0, 1, 1), Vector1d(0), 1e-10))) {
+    std::cout << "(!(is_approx_equal_abstol(S.block(1, 0, 1, 1), Vector1d(0), "
+                 "1e-3))): "
+              << (!(is_approx_equal_abstol(S.block(1, 0, 1, 1), Vector1d(0),
+                                           1e-3)))
+              << std::endl;
+    std::cout << "S.block(1, 0, 1, 1): " << S.block(1, 0, 1, 1) << std::endl;
+    std::cout << "Vector1d(0): " << Vector1d(0) << std::endl;
+    if (!(is_approx_equal_abstol(S.block(1, 0, 1, 1), Vector1d(0), 1e-3))) {
       n = 2;
     }
-    MatrixXd s_11{S.topLeftCornder(n, n)};
-    MatrixXd s{S.transpose().block(n, 0, S.cols() - n, n)};
-    MatrixXd S_1{S.bottomRightCorner(S.Rows() - n, S.cols() - n)};
+    MatrixXd s_11{S.topLeftCorner(n, n)};
+    MatrixXd s{S.transpose().bottomLeftCorner(m - n, n)};
+    MatrixXd S_1{S.bottomRightCorner(m - n, m - n)};
     MatrixXd q_bar_11{Q_bar.topLeftCorner(n, n)};
-    MatrixXd q_bar{Q_bar.transpose().block(n, 0, Q_hat.cols() - n, n)};
-    MatrixXd Q_1{Q_bar.bottomRightCorner(Q_bar.Rows() - n, Q_bar.cols() - n)};
+    MatrixXd q_bar{Q_bar.transpose().bottomLeftCorner(m - n, n)};
+    MatrixXd Q_1{Q_bar.bottomRightCorner(m - n, m - n)};
 
-    MatrixXd x_bar_11(n, n), x_bar(Q_bar.cols() - n, n);
+    MatrixXd x_bar_11(n, n), x_bar(m - n, n);
     if (n == 1) {
+      std::cout << "In n == 1!" << std::endl;
       // solving equation 3.1
-      x_bar_11 << Solve1By1ContinuousLyapunovEquation(s_11, q_bar11);
+      x_bar_11 << Solve1By1ContinuousLyapunovEquation(s_11, q_bar_11);
       // solving equation 3.2
       MatrixXd lhs{S_1.transpose() +
                    MatrixXd::Identity(S_1.cols(), S_1.rows()) * s_11(0)};
       VectorXd rhs{-q_bar - s * x_bar_11};
-      x_bar << lhs.colPivHouseHolderQr().solve(rhs);
+      x_bar << lhs.colPivHouseholderQr().solve(rhs);
     } else {
-      x_hat_11 << Solve2By2ContinuousLyapunovEquation(s_11, q_hat_11);
+      std::cout << "In n == 2!" << std::endl;
+      x_bar_11 << Solve2By2ContinuousLyapunovEquation(s_11, q_bar_11);
       // solving equation 3.2
       // The equation reads as S_1'*x_bar + x_bar*s_11 = -(q_bar+s*x_bar_11),
-      // where S_1 \in R^mxm; x_bar, q_bar, s \in R^mx2 and s_11, x_bar_11 \in
+      // where S_1 \in R^(m-2)x(m-2); x_bar, q_bar, s \in R^(m-2)x2 and s_11,
+      // x_bar_11 \in
       // R^2x2.
       // We solve the linear equation by vectorization and feeding it into
       // colPivHouseHolderQr().
@@ -131,41 +182,62 @@ MatrixXd SolveReducedContinuousLyapunovEquation(
       //  S_11_vec = [s_11(1,1)*I s_11(2,1)*I; s_11(1,2)*I s_11(2,2)*I] with I
       //  \in R^mxm.
       //
+      //  Now define rhs = S_vec + S_11_vec.
+      //
       //  x_vec = [x_bar[1]' x_bar[2]']' where [i] is the ith column
       //
-      //  c_vec = [(q_bar+s*x_bar_11)[1]' (q_bar+s*x_bar_11)[1]']'
+      //  lhs = - [(q_bar+s*x_bar_11)[1]' (q_bar+s*x_bar_11)[1]']'
       //
       //  Now S_1'*x_bar + x_bar*s_11 = -(q_bar+s*x_bar_11) can be rewritten
       //  into:
       //
-      //  (S_vec + S_11_vec)*x_vec = c_vec.
+      //  rhs*x_vec = lhs.
       //
       //  This expression can be feed into colPivHouseHolderQr() and solved.
       //  Finally, construct x_bar from x_vec.
       //
 
-      int m{S_1.rows()};
-      MatrixXd S_vec(m, m);
-      S_vec << S_1.transpose(), MatrixXd::Zero(m, m), MatrixXd::Zero(m, m),
-          S_1.transpose();
-      MatrixXd S_11_vec(m, m);
-      S_11_vec << s_11(0, 0) * MatrixX::Identity(m, m),
-          s_11(0, 1) * MatrixX::Identity(m, m),
-          s_11(1, 0) * MatrixX::Identity(m, m),
-          s_11(1, 1) * MatrixX::Identity(m, m);
-      C_vec << (-q_bar - s * x_bar_11).col(0), (-q_bar - s * x_bar_11).col(1);
+      MatrixXd S_1_vec(2 * (m - 2), 2 * (m - 2));
+      std::cout << "Before assigning to S_1_vec." << std::endl;
+      std::cout << "S_1_vec cols, rows" << S_1_vec.rows() << ","
+                << S_1_vec.cols() << std::endl;
+      std::cout << "S_1 cols, rows" << S_1.rows() << "," << S_1.cols()
+                << std::endl;
+      S_1_vec << S_1.transpose(), MatrixXd::Zero(m - 2, m - 2),
+          MatrixXd::Zero(m - 2, m - 2), S_1.transpose();
+      MatrixXd s_11_vec(2 * (m - 2), 2 * (m - 2));
+      std::cout << "Before assigning to s_11_vec." << std::endl;
+      s_11_vec << s_11(0, 0) * MatrixXd::Identity(m - 2, m - 2),
+          s_11(0, 1) * MatrixXd::Identity(m - 2, m - 2),
+          s_11(1, 0) * MatrixXd::Identity(m - 2, m - 2),
+          s_11(1, 1) * MatrixXd::Identity(m - 2, m - 2);
+      MatrixXd lhs{S_1_vec + s_11_vec};
 
-      x_bar_vec << (S_vec + S_11_vec).colPivHouseHolderQr().solve(C_vec);
-      x_bar << x_bar_vec.block(0, 0, 1, m), x_bar_vec.block(m, 0, 1, m);
+      VectorXd rhs(2 * (m - 2), 1);
+      std::cout << "Before assigning to rhs." << std::endl;
+      rhs << (-q_bar - s * x_bar_11).col(0), (-q_bar - s * x_bar_11).col(1);
+
+      std::cout << "Before solving." << std::endl;
+      VectorXd x_bar_vec{lhs.colPivHouseholderQr().solve(rhs)};
+      x_bar.row(0) = x_bar_vec.segment(0, m - 2);
+      x_bar.row(1) = x_bar_vec.segment(m - 2, m - 2);
     }
-    // Calculate lhs of equation 3.3
-    Eigen::MatrixXd Q_new{(Q_1 + s * x_bar.transpose + x_bar * s)};
+    std::cout << "After determined part." << std::endl;
 
+    // Set up equation 3.3
+    Eigen::MatrixXd Q_new{
+        (Q_1 + s * x_bar.transpose() + x_bar * s.transpose())};
+
+    // The solution is found recursively.
     MatrixXd X_bar(S.cols(), S.rows());
-    X_bar << x_bar_11, x_bar, x_bar.transpose(),
-        SolveReducedContinuousLyapunovEquation(S_1, Q_new)
 
-            return X_bar;
+    std::cout << "Before recursion call." << std::endl;
+    std::cout << "Matrix S_1: " << S_1 << std::endl;
+    std::cout << "Matrix Q_new: " << Q_new << std::endl;
+    X_bar << x_bar_11, x_bar.transpose(), x_bar,
+        SolveReducedContinuousLyapunovEquation(S_1, Q_new);
+    std::cout << "Return X_bar: " << X_bar << std::endl;
+    return X_bar;
   }
 }
 
--- drake/math/continuous_lyapunov_equation.h
+++ drake/math/continuous_lyapunov_equation.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <Eigen/QR>
+
+#include "drake/common/eigen_types.h"
 
 namespace drake {
 namespace math {
@@ -12,24 +15,24 @@ namespace math {
  * A'X + XA + Q = 0
  * @endverbatim
  *
- * where @param Q is a Hermitian matrix with size equal to @param A.
+ * where @param A is real and square, and @param Q is real, symmetric and of
+ * equal size as @param A.
  *
  * Limitations: Given the Eigenvalues of @param A as lambda_1, ..., lambda_n,
  * then the solution is unique if and only if lambda_i +
  * complexConjugate(lambda_j) != 0 for all i, j.
- * Further, if all lambda_i have negative real parts, and if @param Q is
+ * Further, if all lambda_i are negative, and if @param Q is
  * semi-positive definite, then X is also semi-positive definite [1].
- * TODO(FischerGundlach) Check if there is some implication if Q is positive
- * definite, then X is also positive definite. Otherwise, we may have trivial
- * solutions, i.e. X = 0.
  *
- * The implementation is based on SLICOT routine SB03MD [2].
+ * The implementation is based on SLICOT routine SB03MD [2]. Note the
+ * tranformation
+ * Q = -C.
  *
  * [1] Hammarling, S.J., "Numerical solution of the stable, non-negative
  * definite Lyapunov equation," IMA J. Num. Anal., Vol. 2, pp. 303–325, 1982.
  * [2] http://slicot.org/objects/software/shared/doc/SB03MD.html
  *
- * */
+ */
 
 Eigen::MatrixXd ContinuousLyapunovEquation(
     const Eigen::Ref<const Eigen::MatrixXd>& A,
@@ -39,14 +42,16 @@ namespace internal {
 
 // Subroutines which help special cases.
 
-Eigen::Vector1d Solve1By1ContinuousLyapunovEquation(const Eigen::Ref<const Eigen::Vector1d> &A,
-                                                    const Eigen::Ref<const Eigen::Vector1d> &Q);
+Vector1d Solve1By1ContinuousLyapunovEquation(
+    const Eigen::Ref<const Vector1d>& A, const Eigen::Ref<const Vector1d>& Q);
 
-Eigen::Matrix2d Solve2By2ContinuousLyapunovEquation(const Eigen::Ref<const Eigen::Matrix2d> &A,
-                                                    const Eigen::Ref<const Eigen::Matrix2d> &Q);
+Eigen::Matrix2d Solve2By2ContinuousLyapunovEquation(
+    const Eigen::Ref<const Eigen::Matrix2d>& A,
+    const Eigen::Ref<const Eigen::Matrix2d>& Q);
 
-Eigen::MatrixXd SolveReducedContinuousLyapunovEquation(const Eigen::Ref<const Eigen::MatrixXd> &A,
-                                                    const Eigen::Ref<const Eigen::MatrixXd> &Q);
+Eigen::MatrixXd SolveReducedContinuousLyapunovEquation(
+    const Eigen::Ref<const Eigen::MatrixXd>& A,
+    const Eigen::Ref<const Eigen::MatrixXd>& Q);
 
 }  // namespace internal
 }  // namespace math
